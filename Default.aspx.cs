ab04fd5bea9569c3f04d2754c32aba7057a672df
David Andrew <damussel06@hotmail.com>
2016-10-10 15:22:42 -0500

    basic funtionality complete.

--- Default.aspx.cs
+++ Default.aspx.cs
@@ -16,7 +16,11 @@ namespace CS_ASP_019
 
         protected void submitButton_Click(object sender, EventArgs e)
         {
-            String result = String.Format("Thank you {0}, for your business", nameTextBox.Text);
+            String result = String.Format("Thank you {0}, for your business<br>" +
+                "Your phone number is {1}<br>" +
+                "Your social security number is {2}<br>" +
+                "The loan date you picked is {3}<br>" +
+                "The amount you want to borrow is: {4}", nameTextBox.Text, phoneTextBox.Text, ssTextBox.Text, loanDateCalendar.SelectedDate, salaryTextBox.Text);
 
             if (result == "")
             {
