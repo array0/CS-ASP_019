1ced99d46bfe467e243f3534a305ee1e374cdb5e
David Andrew <damussel06@hotmail.com>
2016-10-11 09:22:47 -0500

    Complete.
    Had to change the data type, or some phone numbers would cause an
    overflow.

--- Default.aspx.cs
+++ Default.aspx.cs
@@ -16,12 +16,13 @@ namespace CS_ASP_019
 
         protected void submitButton_Click(object sender, EventArgs e)
         {
-            int phone = int.Parse(phoneTextBox.Text);
+            double phone = double.Parse(phoneTextBox.Text);
             int ss = int.Parse(ssTextBox.Text);
+            double salary = double.Parse(salaryTextBox.Text);
             
 
             String result = String.Format("Thank you {0}, for your business<br>" +
-                "Your phone number is {1:(000)000-0000}<br>" +
+                "Your phone number is {1: (000) 000-0000}<br>" +
                 // "Your phone number is {1}<br>" +
                 "Your social security number is {2:000-00-0000}<br>" +
                 "The loan date you picked is {3:MMM d, yyyy}<br>" +
@@ -30,7 +31,7 @@ namespace CS_ASP_019
                 phone,
                 ss,
                 loanDateCalendar.SelectedDate,
-                salaryTextBox.Text);
+                salary);
 
             if (result == "")
             {
